e6bb2d20daba0d1c0bfb3dd2765d0cd9522ca2ca
noblevk21 <noblevk21@gmail.com>
2024-01-22 19:15:03 +0530

    adding love calculator and Calendar

---
+++ Day 3/lovecalculator.py
@@ -0,0 +1,24 @@
+print("The love calculator is calculating your score:")
+
+
+name1 = input("Enter name1: ")
+name2 = input("Enter name2: ")
+
+combined_names = name1 + name2
+lower_names = combined_names.lower()
+
+t = lower_names.count("t")
+r = lower_names.count("r")
+u = lower_names.count("u")
+e = lower_names.count("e")
+
+first_digit = t + r + u + e
+
+l = lower_names.count("l")
+o = lower_names.count("o")
+v = lower_names.count("v")
+e = lower_names.count("e")
+second_digit = l + o + v + e
+
+score = int(str(first_digit) + str(second_digit))
+print(f"Love Score: {score}")
