09ca52d3e1db0e3f1efc931ab06bbda3c6605833
noblevk21 <noblevk21@gmail.com>
2024-01-25 23:13:04 +0530

    arthmetic

---
+++ Day 1/arthmeticops.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the first integer: "))
+b = int(input("Enter the second integer: "))
+
+# Calculate the sum, difference, and product
+sum_result = a + b
+difference_result = a - b
+product_result = a * b
+
+# Print the results
+print(sum_result)
+print(difference_result)
+print(product_result)
