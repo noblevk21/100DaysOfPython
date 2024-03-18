71a44b047c45c4236f6b34421becbf6336925eb1
noblevk21 <noblevk21@gmail.com>
2024-03-18 08:47:21 +0530

    fizzbuzz

---
+++ Day 5/fizzbuzz.py
@@ -0,0 +1,11 @@
+value = int(input (" Enter the target number:"))
+
+for number in range(1, value + 1):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
